d2ee0218a8155d5876d29b2b9f2812a4f981b4f5
Yaroslav-Sasim <62548404+Yaroslav-Sasim@users.noreply.github.com>
2021-04-15 19:43:45 +0300

    Simple Timer
    Timer on C++

---
+++ Project2/MyForm.h
@@ -0,0 +1,145 @@
+#pragma once
+
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+
+		
+	    int Second = 0;
+		int Minute = 0;
+		String^ Sec;
+		String^ Min;
+
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(141, 68);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(58, 33);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"0:0";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Blue;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::Color::Yellow;
+			this->button1->Location = System::Drawing::Point(112, 127);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(118, 33);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Start";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// timer1
+			// 
+			this->timer1->Interval = 1000;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(339, 299);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		timer1->Enabled = true;
+		if (timer1->Enabled == true)
+		{
+			button1->Text = "Stop";
+		}
+		
+		if ((button1->Text == "Stop")&&(Second>0)&&(Minute>=0))
+		{
+			timer1->Enabled = false;
+			Second = 0;
+			Minute = 0;
+			Sec = Convert::ToString(Second);
+			Min = Convert::ToString(Minute);
+			button1->Text = "Start";
+
+		}
+		
+	}
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+		Second++;
+		if (Second == 60)
+		{
+			Second = 0;
+			Minute++;
+		}
+		Sec = Convert::ToString(Second);
+		Min = Convert::ToString(Minute);
+		label1->Text = Min + ":" + Sec;
+
+	}
+};
+}
